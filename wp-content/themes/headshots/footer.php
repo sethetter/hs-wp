becdecbba5238f54c817bc8f6f1e5bc3807d1a48
Seth Etter <sethetter@gmail.com>
2013-12-12 21:25:00 -0600

    Included bootstrap and jquery in header/footer

--- wp-content/themes/headshots/footer.php
+++ wp-content/themes/headshots/footer.php
@@ -1,3 +1,6 @@
   </div>
+  <script src="<?php get_template_directory(); ?>/vendor/jquery.min.js" type="text/javascript"></script>
+  <script src="<?php get_template_directory(); ?>/vendor/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+  <?php wp_footer(); ?>
 </body>
 </html>
--- wp-content/themes/headshots/header.php
+++ wp-content/themes/headshots/header.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
   <title><?php wp_title(); ?></title>
+  <link href="<?php get_template_directory(); ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <?php wp_head(); ?>
 </head>
 <body>
   <div class="container">
