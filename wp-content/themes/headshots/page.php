d77083f5aa2bbe70494fae122685542a0eba0a03
Seth Etter <sethetter@gmail.com>
2013-12-12 22:46:10 -0600

    Page loop in place, htaccess filled out

--- wp-content/themes/headshots/page.php
+++ wp-content/themes/headshots/page.php
@@ -1,3 +1,19 @@
 <?php get_header(); ?>
+<?php 
+  if ( have_posts() ) {
+    while ( have_posts() ) {
+      the_post(); 
+?>
 
+<!-- Page content -->
+<h1><?php the_title(); ?></h1>
+
+<div class="page-content">
+  <?php the_content(); ?>
+</div>
+
+<?php 
+    } // end while
+  } // end if
+?>
 <?php get_footer(); ?>
