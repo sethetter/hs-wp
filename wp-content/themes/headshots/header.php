cfd6dbd9875c21abe13143d7324701090a52e4c5
Seth Etter <sethetter@gmail.com>
2013-12-12 22:37:30 -0600

    Logo image included, carousel issue fixed

--- wp-content/themes/headshots/header.php
+++ wp-content/themes/headshots/header.php
@@ -30,7 +30,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Project name</a>
+            <a id="logo" class="navbar-brand" href="#">Headshots Bar &amp; Grill</a>
           </div>
           <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
