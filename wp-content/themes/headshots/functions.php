bd2f8bb4db00350ccfb69d6a3a8c4bf6d24e6af8
Seth Etter <sethetter@gmail.com>
2014-03-21 18:31:25 -0500

    Functions file with custom post types

---
+++ wp-content/themes/headshots/functions.php
@@ -0,0 +1,57 @@
+<?php
+
+add_theme_support('post-thumbnails');
+
+add_action('init', create_food_post_type);
+add_action('init', create_drinks_post_type);
+add_action('init', create_games_post_type);
+add_action('init', create_events_post_type);
+
+function create_food_post_type() {
+  register_post_type('food', array(
+    'label' => 'Food',
+    'public' => true,
+    'menu_position' => 5,
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'taxonomies' => array('category')
+  ));
+
+  register_taxonomy('category', 'food');
+}
+
+function create_drinks_post_type() {
+  register_post_type('drinks', array(
+    'label' => 'Drinks',
+    'public' => true,
+    'menu_position' => 5,
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'taxonomies' => array('category')
+  ));
+
+  register_taxonomy('category', 'drinks');
+}
+
+function create_games_post_type() {
+  register_post_type('games', array(
+    'label' => 'Games',
+    'public' => true,
+    'menu_position' => 5,
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'taxonomies' => array('category')
+  ));
+
+  register_taxonomy('category', 'games');
+}
+
+function create_events_post_type() {
+  register_post_type('events', array(
+    'label' => 'Events',
+    'public' => true,
+    'menu_position' => 5,
+    'supports' => array('title', 'editor', 'thumbnail', 'custom-fields'),
+    'taxonomies' => array('category')
+  ));
+
+  register_taxonomy('category', 'events');
+}
+?>
