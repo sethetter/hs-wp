be6e89924ed5824ca48bec380519be3e1da78958
Seth Etter <sethetter@gmail.com>
2014-03-21 19:17:23 -0500

    Content page started

--- wp-content/themes/headshots/page-about-us.php
+++ wp-content/themes/headshots/page-about-us.php
@@ -2,9 +2,21 @@
 <?php get_header(); ?>
 <!-- HEADER -->
 
-
 <!-- BODY -->
+<div class="page container">
+  <?php if (have_posts()) : while (have_posts()) : the_post();?>
 
+    <div class="row title">
+      <div class="col-lg-12">
+        <h1><?php the_title(); ?></h1>
+      </div>
+    </div>
+
+    <div class="row content">
+    </div>
+
+  <?php endwhile; endif; ?>
+</div>
 <!-- BODY -->
 
 
