04615a5d23133067c71f7567235d154f70f2ffc6
Seth Etter <sethetter@gmail.com>
2014-04-05 17:20:53 -0500

    Brought back events page

---
+++ wp-content/themes/headshots/page-events.php
@@ -0,0 +1,28 @@
+<!-- HEADER -->
+<?php get_header(); ?>
+<!-- HEADER -->
+
+<!-- BODY -->
+<div class="page container">
+  <?php if (have_posts()) : while (have_posts()) : the_post();?>
+
+    <div class="row title">
+      <div class="col-lg-12">
+        <h1><?php the_title(); ?></h1>
+      </div>
+    </div>
+
+    <div class="row content">
+      <div class="col-lg-12">
+        <?php the_content(); ?>
+      </div>
+    </div>
+
+  <?php endwhile; endif; ?>
+</div>
+<!-- BODY -->
+
+
+<!-- FOOTER -->
+<?php get_footer(); ?>
+<!-- FOOTER -->
