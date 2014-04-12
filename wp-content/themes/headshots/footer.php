e88ac8a9ec1a2c9e15e1dd4da3c1cc901836be53
Landon Cline <landonc@hightouchinc.com>
2014-04-12 17:18:27 -0500

    updates to footer for social media

--- wp-content/themes/headshots/footer.php
+++ wp-content/themes/headshots/footer.php
@@ -1,4 +1,18 @@
   </div>
+  <hr>
+  <div id="socialMedia" class="container">
+    <h6>FOLLOW US</h6>
+      <ul>
+        <li class=""><a href="https://twitter.com/headshotsbar">Follow Headshots on Twitter</a></li>
+        <li class="k"><a href="https://www.facebook.com/HeadshotsWichita">Be a fan on FaceBook</a></li>
+        <li class=""><a href="http://www.youtube.com/headshotsbar">Check out our video event clips on YouTube</a></li>
+      </ul>
+  </div>
+  <div id="copyright" class="container">
+    <p class="pull-right"><a href="#">Back to top</a></p>
+    <p>&copy; 2013 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+  </div>
+
   <script src="<?php bloginfo('template_directory'); ?>/vendor/jquery.min.js" type="text/javascript"></script>
   <script src="<?php bloginfo('template_directory'); ?>/vendor/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
   <?php wp_footer(); ?>
